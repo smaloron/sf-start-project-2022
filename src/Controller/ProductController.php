fddb2bc27f87f88b2310a632bee0b1e04d1d1b3a
seb <contact@smaloron.eu>
2022-03-08 15:57:56 +0100

    CRUD sur les produits avec Doctrine

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,11 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
 use App\Service\ProductService;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -65,6 +71,76 @@ class ProductController extends AbstractController {
             "title" => "Liste des produits dans la catégorie : $category",
             "categoryList" => $this->repository->getDistinctCategories(),
             "currentCategory" => $category
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id<\d+>}", name="product_delete")
+     * @param int $id
+     * @param EntityManagerInterface $manager
+     * @return RedirectResponse|NotFoundHttpException
+     */
+    public function deleteOne(int $id, EntityManagerInterface $manager): RedirectResponse|NotFoundHttpException{
+        $product = $this->repository->findOneBy(["id" => $id]);
+        if(! $product) {
+            return $this->createNotFoundException("Ce produit n'existe pas");
+        }
+
+        $manager->remove($product);
+
+        $manager->flush();
+
+        return $this->redirectToRoute("product_list");
+
+    }
+
+    /**
+     * @Route("/new", name="product_new")
+     * @Route("/edit/{id<\d+>}", name="product_edit")
+     */
+    public function addOrEditProduct(
+        Request $request,
+        EntityManagerInterface $manager,
+        int $id = null): NotFoundHttpException|RedirectResponse|Response
+    {
+        if($id !== null){
+            // Récupération d'un produit existant
+            $product = $this->repository->findOneById($id);
+
+            // test de produit non trouvé
+            if(! $product){
+                return $this->createNotFoundException("Produit non trouvé");
+            }
+        } else {
+            // Instanciation d'un produit vide
+            $product = new Product();
+        }
+
+        // Création du formulaire
+        $form = $this->createForm(
+            ProductType::class,
+            $product
+        );
+
+        // Hydratation du formulaire avec la requête HTTP
+        // Cette opération hydrate également l'éventuelle
+        // entité liée et effectue les validations définies
+        // au niveau du formulaire ou de l'entité
+        $form->handleRequest($request);
+
+        // Traitement du formulaire si celui-ci est posté
+        // et qu'il est valide
+        if ($form->isSubmitted() && $form->isValid()){
+            // Sauvegarde de l'entité dans la BD
+            $manager->persist($product);
+            $manager->flush();
+
+            // Redirection vers la liste des produits
+            return $this->redirectToRoute("product_list");
+        }
+
+        return $this->render("product/form.html.twig", [
+            "productForm" => $form->createView()
         ]);
     }
 
